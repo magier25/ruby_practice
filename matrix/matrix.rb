563662dbc1a00ff970a9b65e8be845007cf1b9a2
magier25 <magier25>
2021-03-07 19:27:15 +0100

    Matrix

---
+++ matrix/matrix.rb
@@ -0,0 +1,26 @@
+class Matrix
+  attr_reader :rows, :columns
+
+  def initialize(m)
+    create_matrix(m)
+  end
+
+  private
+
+  def create_matrix(m)
+    @rows = m.split("\n").each_with_object([]) do |row, rows|
+      rows << row.split(" ").each_with_object([]) do |item, arr|
+        arr << item.to_i
+      end
+    end
+
+    columns_number = rows[0].size
+    @columns = []
+    for i in 0...columns_number
+      @columns << []
+      for j in 0...rows.size
+        @columns[i] << rows[j][i]
+      end
+    end
+  end
+end
