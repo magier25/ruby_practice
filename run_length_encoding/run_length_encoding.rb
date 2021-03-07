46de45b83cd08af1e3cc80b70a4c224406d2a887
magier25 <magier25>
2021-03-07 21:51:03 +0100

    Brilliat solution for Run length encoding using regexp

--- run_length_encoding/run_length_encoding.rb
+++ run_length_encoding/run_length_encoding.rb
@@ -56,3 +56,18 @@ class RunLengthEncoding
   end
 
 end
+
+
+=begin
+# excellet solution
+
+class RunLengthEncoding
+  def self.encode(plaintext)
+    plaintext.gsub(/(.)\1+/) { |group| "#{group.length}#{group[0]}" }
+  end
+
+  def self.decode(code)
+    code.gsub(/(\d+)(\D)/) { Regexp.last_match(2) * Regexp.last_match(1).to_i }
+  end
+end
+=end
