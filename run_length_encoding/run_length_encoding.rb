9a32d73d136c79b06a7b5b8df56a6db95f2f6e6e
magier25 <magier25>
2021-03-07 21:39:51 +0100

    Run length encoding

---
+++ run_length_encoding/run_length_encoding.rb
@@ -0,0 +1,58 @@
+class RunLengthEncoding
+  def self.encode(input)
+    return '' if input.empty?
+
+    result = {
+      output: '',
+      letter: '',
+      buf: '',
+    }
+
+    input.split("").each_with_object(result) do |letter, obj|
+      if obj[:letter] == ''
+        obj[:letter] = letter
+        obj[:buf] += letter
+      elsif obj[:letter] == letter
+        obj[:buf] += letter
+      else
+        obj[:output] += code_letter(obj[:buf].size, obj[:letter])
+        obj[:buf] = letter
+        obj[:letter] = letter
+      end
+    end
+
+    result[:output] += code_letter(result[:buf].size, result[:letter])
+    
+    return result[:output]
+
+  end
+
+  def self.decode(input)
+    result = ''
+    until input.empty?
+      m = input.match(/^(?<number>\d+)(?<letter>[\w ])(?<rest>.*)/)
+      if m
+        result += (m[:letter] * m[:number].to_i)
+        input = m[:rest]
+        next
+      end
+
+      m = input.match(/^(?<letter>[\w ])(?<rest>.*)/)
+      if m
+        result += m[:letter]
+        input = m[:rest]
+      end
+    end
+
+    result
+  end
+
+  private
+
+  def self.code_letter(num, letter)
+    return letter if num == 1
+
+    "#{num}#{letter}"
+  end
+
+end
